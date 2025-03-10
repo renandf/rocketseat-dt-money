abbd4362aa9f4dcdb1162515be6f6716eb0e4d2e
Renan Castro <renandf@gmail.com>
2025-03-10 18:56:56 +1000

    feat: Add non-functional styled ledger table

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,44 @@
 import { Header } from '../../components/Header'
 import { Summary } from '../../components/Summary'
+import { Price, TransactionsContainer, TransactionsTable } from './styles'
 
 export function Transactions() {
   return (
     <div>
       <Header />
       <Summary />
+
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td>Activity</td>
+              <td>
+                <Price variant="income">$12,000.00</Price>
+              </td>
+              <td>Sell</td>
+              <td>13 Apr 2025</td>
+            </tr>
+            <tr>
+              <td>Activity</td>
+              <td>
+                <Price>-$8,000.00</Price>
+              </td>
+              <td>Sell</td>
+              <td>13 Apr 2025</td>
+            </tr>
+            <tr>
+              <td>Activity</td>
+              <td>
+                <Price variant="income">$12,000.00</Price>
+              </td>
+              <td>Sell</td>
+              <td>13 Apr 2025</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
+
     </div>
   )
 }
---
+++ src/pages/Transactions/styles.ts
@@ -0,0 +1,44 @@
+import styled from 'styled-components';
+
+export const TransactionsContainer = styled.main`
+  margin: 4rem auto 0;
+  padding-inline: 1.5rem;
+  width: 100%;
+  max-width: ${props => props.theme['container-content-max-width']};
+  `
+
+export const TransactionsTable = styled.table`
+  width: 100%;
+  border-collapse: separate;
+  border-spacing: 0 .5rem;
+
+  td {
+    padding: 1.25rem 2rem;
+    background-color: ${props => props.theme['gray-700']};
+
+    &:first-child {
+      width: 40%;
+      border-start-start-radius: ${props => props.theme['border-radius-md']};
+      border-end-start-radius: ${props => props.theme['border-radius-md']};
+    }
+
+    &:last-child {
+      width: 40%;
+      border-start-end-radius: ${props => props.theme['border-radius-md']};
+      border-end-end-radius: ${props => props.theme['border-radius-md']};
+    }
+  }
+`
+
+interface PriceProps {
+  variant?: 'income' | 'expense';
+}
+
+export const Price = styled.span<PriceProps>`
+
+  color: ${
+    props => props.variant === 'income'
+    ? props.theme['green-300']
+    : props.theme['red-200']
+  }
+`
